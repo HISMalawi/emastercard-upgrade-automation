355d92ae45e7d902cb8cc42100743ed06fe9699a
mnaboti <mnaboti@pedaids.org>
2025-06-13 10:51:16 +0200

    v2025.Q1.R6

---
+++ web/static/assets/program_report_service-6f21b364.js
@@ -0,0 +1,1 @@
+import{R as t}from"./report_service-1e6f45bf.js";class p extends t{constructor(){super()}generate(r,e){return this.getReport("/programs/".concat(this.programId,"/reports/").concat(r),e)}}export{p as P};
