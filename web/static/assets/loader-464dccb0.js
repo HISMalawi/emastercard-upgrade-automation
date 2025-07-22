c2518554dca29c53238c5eeae34b444f799249aa
Kyton Thaundi <kythaundi@gmail.com>
2025-07-22 10:18:42 +0200

    refactor q2 changes

---
+++ web/static/assets/loader-464dccb0.js
@@ -0,0 +1,1 @@
+import{bu as n}from"./index-cea95aa1.js";const r={async show(e){return(await n.create({message:e||"Loading...",spinner:"crescent",translucent:!0})).present()},async hide(){n.getTop().then(e=>e?n.dismiss():null)}};export{r as l};
--- web/static/assets/status-tap-legacy-62c546f1.js
+++
@@ -1,5 +0,0 @@
-System.register(["./index-legacy-79e582e4.js"],function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.be,r=e.bf,s=e.bg,i=e.bh,o=e.bi}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-e("startStatusTap",()=>{const e=window;e.addEventListener("statusTap",()=>{n(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise(e=>s(c,e)).then(()=>{i(async()=>{c.style.setProperty("--overflow","hidden"),await o(c,300),c.style.removeProperty("--overflow")})})})})})}}});
