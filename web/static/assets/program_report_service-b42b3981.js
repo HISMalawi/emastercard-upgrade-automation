f52f8fbc55aac94e4d900ccb82fd24edfd76e23f
Daniel Justin <danieljmuleke@gmail.com>
2026-02-19 10:17:09 +0200

    v2025.Q4.R5-streaming

---
+++ web/static/assets/program_report_service-b42b3981.js
@@ -0,0 +1,1 @@
+import{R as t}from"./report_service-43b2d662.js";class p extends t{constructor(){super()}generate(r,e){return this.getReport("/programs/".concat(this.programId,"/reports/").concat(r),e)}}export{p as P};
